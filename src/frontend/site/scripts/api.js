54e395b52701504befad28e12c1572b4ad1f10d2
victorxaviercosta <151531232+victorxaviercosta@users.noreply.github.com>
2025-06-25 08:43:17 -0300

    fixed cookies
    site must be accessed from: http://localhost:8000

--- src/frontend/site/scripts/api.js
+++ src/frontend/site/scripts/api.js
@@ -11,26 +11,19 @@
 const API_BASE = "http://localhost:8000/api";
 
 export async function api(endpoint, method = "GET", body = null) {
-  const options = {
-    method,
-    headers: { "Content-Type": "application/json" },
-  };
-  if (body) {
-    options.body = JSON.stringify(body);
-  }
+	const options = {
+    	method: method,
+    	headers: { "Content-Type": "application/json" },
+		credentials: "include"
+  	};
+  	if (body) {
+    	options.body = JSON.stringify(body);
+  	}
 
-  const res = await fetch(`${API_BASE}${endpoint}`, options);
-  if (!res.ok) {
-    const err = await res.json().catch(() => ({}));
-    throw new Error(err.detail || Error `${res.status}`);
-  }
-  return res.json();
+  	const res = await fetch(`${API_BASE}${endpoint}`, options);
+  	if (!res.ok) {
+    	const err = await res.json().catch(() => ({}));
+    	throw new Error(err.detail || Error `${res.status}`);
+  	}
+  	return res.json();
 }
-
-/*
-api("/hangar")
-        .then(data => {
-            communicate(data.msg);
-        })
-        .catch(err => console.error(err));
-*/
