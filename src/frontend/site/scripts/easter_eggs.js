2809ff8f86156568898ff302d5e2c9333930cbe9
Andrei <andreimiguelcristeli2015@gmail.com>
2025-07-27 14:06:11 -0300

    easter egg massa

--- src/frontend/site/scripts/easter_eggs.js
+++ src/frontend/site/scripts/easter_eggs.js
@@ -22,7 +22,9 @@ export function showPokemonDialog() {
   	const modal = document.createElement("dialog");
   	modal.className = "pokemon-dialog";
   	modal.innerHTML = `
-    	<a href="https://monkepo.online/" target="_blank"> <img src="https://monkepo.online/img/monkepo.png" alt="Monképo"> </a>
+    	<a href="https://youtu.be/L9gl6yBA7wU?si=Q47KV7mFLSi6gqju" target="_blank"> 
+			<img src="https://pt.quizur.com/_image?href=https%3A%2F%2Fimg.quizur.com%2Ff%2Fimg5f7340bb214684.72884410.png%3FlastEdited%3D1601388742&w=600&h=600&f=webp" 
+			alt="Monképo"></a>
     	<button class="closeDialog milvus" aria-label="Fechar">&times;</button>
   	`;
   	document.body.appendChild(modal);
