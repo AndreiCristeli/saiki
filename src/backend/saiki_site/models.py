87619dcd2880ef404624670df8a0598888dc2ca2
HexagonalUniverse <hebertluizhd78@hotmail.com>
2025-07-29 19:39:55 -0300

    stuff have been moved...

--- src/backend/saiki_site/models.py
+++ src/backend/saiki_site/models.py
@@ -5,12 +5,6 @@ Django models used by the site.
 """
 
 from django.db import models
-
-
-"""
-(Initially on src/backend/db/models.py. 
-@TODO: CHANGE IT TO THERE, EVENTUALLY)
-"""
 from django.utils import timezone
 from django.db import models
 from django.db.models import CharField, IntegerField
@@ -18,37 +12,7 @@ from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.models import User
 
-#------------------------------------------------Model Algorithm---------------------------------------------------------------------
-class Model_Algorithm(models.Model):
-    """Represents an algorithm; the entity."""
 
-    year = IntegerField()
-    name = CharField(max_length=32)
-    year = IntegerField()
-    category = CharField(max_length=32)
-    data_structures = CharField(max_length=512)
-    design_paradigm = CharField(max_length=32)
-    generality = CharField(max_length=32)
-    temporal_complexity = CharField(max_length=32)
-    spatial_complexity = CharField(max_length=32)
-    solution_kind = CharField(max_length=32)
-
-    def to_json(self) -> JsonResponse:
-        data: dict[str, CharField] = {
-            "name": self.name,
-            "year": self.year,
-            "category": self.category,
-            "design_paradigm": self.design_paradigm,
-            "generality": self.generality,
-            "temporal_complexity": self.temporal_complexity,
-            "spatial_complexity": self.spatial_complexity,
-            "solution_kind": self.solution_kind,
-        }
-
-        return JsonResponse(data)
-
-
-#------------------------------------------------------------------------------------------------------------------------------------
 class Jogador(models.Model):
     """Represents a player; the entity."""
 
